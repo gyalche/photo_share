1f7468c9014ff14d5a6ad45c855cd306e2ec2e39
Dawa <sherpadawa01@outlook.com>
2022-05-07 23:16:45 +0545

    backend sanity

---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,20 @@
+export default{
+    name:'save',
+    title:'Save',
+    type:'document',
+
+    fields:[
+        {
+            name:'postedBy',
+            title:'postedBy',
+            type:'postedBy',
+
+        },
+        {
+            name:'userId',
+            title:'userId',
+            type:'string',
+        }
+    ]
+
+}
